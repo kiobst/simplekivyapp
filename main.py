dbfdbf7b8b6bed3bf4f2d9bb3bac520e568ccbc2
Kekoa <kekoaponolani@gmail.com>
2017-01-12 13:15:55 -0500

    added multi screens, drawing app, end of tutorial

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+
 from kivy.uix.widget import Widget
 from kivy.graphics import Line
 
-class DrawInput(Widget):
+
+class Painter(Widget):
     def on_touch_down(self, touch):
-        print(touch)
         with self.canvas:
             touch.ud["line"] = Line(points=(touch.x, touch.y))
 
     def on_touch_move(self, touch):
-        print(touch)
-        touch.ud["line"].points += (touch.x, touch.y)
+        touch.ud["line"].points += [touch.x, touch.y]
 
-    def on_touch_up(self, touch):
-        print("Released!", touch)
+
+class MainScreen(Screen):
+    pass
+
+
+class AnotherScreen(Screen):
+    pass
+
+
+class ScreenManagement(ScreenManager):
+    pass
+
+
+presentation = Builder.load_file("simplekivy.kv")
+
 
 class SimpleKivy(App):
     def build(self):
-        return DrawInput()
+        return presentation
 
 if __name__ == "__main__":
     SimpleKivy().run()

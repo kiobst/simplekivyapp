2c11b8afba2cee24063d57dcb44f328fdea1389f
Kekoa <kekoaponolani@gmail.com>
2017-01-12 12:53:45 -0500

    simple drawing app

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from kivy.app import App
-from kivy.uix.label import Label
 from kivy.uix.widget import Widget
+from kivy.graphics import Line
 
+class DrawInput(Widget):
+    def on_touch_down(self, touch):
+        print(touch)
+        with self.canvas:
+            touch.ud["line"] = Line(points=(touch.x, touch.y))
 
-class Widgets(Widget):
-    pass
+    def on_touch_move(self, touch):
+        print(touch)
+        touch.ud["line"].points += (touch.x, touch.y)
 
+    def on_touch_up(self, touch):
+        print("Released!", touch)
 
 class SimpleKivy(App):
     def build(self):
-        return Widgets()
+        return DrawInput()
 
 if __name__ == "__main__":
     SimpleKivy().run()
